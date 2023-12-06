16ab480b9bc4318c27c985fa5452051f38c8207a
joha152 <johanna.servodio@hotmail.com.ar>
2023-12-06 15:49:09 -0300

    firts commit

---
+++ src/assets/Components/Price/Price.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Price = ()=>{
+
+    return(
+
+        <>
+        
+        
+        </>
+    )
+}
+
+
+export default Price
---
+++ src/assets/Components/Service/Service.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Service = () => {
+
+    return (
+        <>
+            <h1>SERVIÇOS</h1>
+            <h3>Desenvolvimento de software personalizado</h3>
+            <p>Oferecer serviços de desenvolvimento de software customizado para empresas, adaptando soluções às necessidades específicas de cada cliente. Isso pode incluir sistemas de gerenciamento de estoque, pontos de venda personalizados, integrações com plataformas de comércio eletrônico e outras soluções projetadas para melhorar a eficiência e a produtividade dos negócios.</p>
+
+            <h3>Implementação e treinamento</h3>
+            <p>Fornecer serviços de implementação de software no ambiente do cliente, garantindo que a transição seja tranquila e contínua.
+                Além disso, forneça treinamento detalhado aos usuários para garantir que eles aproveitem ao máximo os recursos do software. Isto incluiria treinamento no uso diário, administração do sistema e solução de problemas básicos.</p>
+
+            <h3>Suporte Técnico e Manutenção Contínua:</h3>
+            <p>Fornecer serviços de suporte técnico contínuos para garantir a operação ininterrupta do software. Isso pode incluir suporte
+                por telefone, suporte online, atualizações de software e manutenção preventiva. Um atendimento ao cliente eficiente e proativo pode fazer toda a diferença para que os clientes se sintam apoiados e satisfeitos com a solução.</p>
+
+                <button>EXPERIMENTE GRATUITAMENTE 7 DIAS</button>
+        </>
+    )
+}
+
+export default Service
